f0713e207cc49fa978aeb680e3c80cd664e4aa44
programacion <>
2021-06-02 13:50:39 +0200

    na

--- src/ensayo/EnsayoDao.java
+++ src/ensayo/EnsayoDao.java
@@ -10,11 +10,13 @@ public class EnsayoDao {
 	public static void main(String[] args) {
 		Facade facade=new Facade();
 //		facade.add(new Persona(1, "Fulgencio"));
+		Persona persona = facade.getPersona(0);
+		
 //		facade.add(new ArticuloDTO(1, "tornillaco"));
 //		ArticuloDTO buscarArticuloDTO = facade.buscarArticuloDTO(1);
-		CancionDTO cancion = new CancionDTO("adios",112, true);
+//		CancionDTO cancion = new CancionDTO("adios",112, true);
 //		facade.add(cancion);
-		CancionDTO otra=facade.getCancion(2);
-		System.out.println(otra.toString());
+//		CancionDTO otra=facade.getCancion(2);
+//		System.out.println(otra.toString());
 	}
 }
